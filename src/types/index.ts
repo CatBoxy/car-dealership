d71554cb2f8470d24e8abc0b5e5f304e0043a93e
CatBoxy <thecatbox@protonmail.com>
2024-08-13 20:54:30 -0300

    new ModeYearResponse type

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,3 +11,17 @@ export type VehicleResponse = {
   SearchCriteria: string;
   Results: VehicleResult[];
 };
+
+export type ModelYearResult = {
+  Make_ID: number;
+  Make_Name: string;
+  Model_ID: number;
+  Model_Name: string;
+};
+
+export type ModelYearResponse = {
+  Count: number;
+  Message: string;
+  SearchCriteria: string;
+  Results: ModelYearResult[];
+};
