325b5ad85d4cde0bbbd6d0b20cebb8cdd4db359d
CatBoxy <thecatbox@protonmail.com>
2024-08-13 20:54:51 -0300

    added getModelsByMakeAndYear api endpoint

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,27 @@
-import { VehicleResponse } from "@/types";
+import { ModelYearResponse, VehicleResponse } from "@/types";
+
+const BASE_URL = process.env.API_BASE_URL!;
 
 export async function getModels(): Promise<VehicleResponse> {
-  const response = await fetch(process.env.API_URL!);
+  const response = await fetch(
+    `${BASE_URL}/api/vehicles/GetMakesForVehicleType/car?format=json`
+  );
   if (!response.ok) {
     throw new Error("Failed to fetch models");
+  }
+  return response.json();
+}
+
+export async function getModelsByMakeAndYear(
+  makeId: string,
+  year: string
+): Promise<ModelYearResponse> {
+  const response = await fetch(
+    `${BASE_URL}/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
+  );
+  if (!response.ok) {
+    console.log(response);
+    throw new Error("Failed to fetch models for make and year");
   }
   return response.json();
 }
